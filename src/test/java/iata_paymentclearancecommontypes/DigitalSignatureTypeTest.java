33e26c1401fa2ff7fc42382310365cbfa24dada7
Jin Kwon <onacit@gmail.com>
2022-11-20 15:31:28 +0900

    wip

---
+++ src/test/java/iata_paymentclearancecommontypes/DigitalSignatureTypeTest.java
@@ -0,0 +1,12 @@
+package iata_paymentclearancecommontypes;
+
+import org.iata.iata._2015._00._2021_3.iata_paymentclearancecommontypes.DigitalSignatureType;
+import org.junit.jupiter.api.Test;
+
+class DigitalSignatureTypeTest {
+
+    @Test
+    void construct() {
+        final var constructed = new DigitalSignatureType();
+    }
+}
